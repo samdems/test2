8e7edd700d5f5a2c936660c4cc24a3608ec782ee
sam cross <sam.cross@4seconds.co.uk>
2023-06-01 10:45:14 +0100

    added vue js

--- new-sail-application/resources/js/app.js
+++ new-sail-application/resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import {createApp} from 'vue/dist/vue.esm-bundler.js';
+import Home from './components/Home.vue';
+
+
+const app = createApp({});
+
+app.component('home-component', Home);
+
+app.mount("#app");
+                                                        
+                                                    
+                                                            
+                                                        
---
+++ new-sail-application/resources/js/components/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>
+        How to Add Vue js 3 to Laravel 10 with Vite
+    </h1>
+</template>
